8cfce655969b1a996206ed5d59f48634722470ff
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-03 15:27:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,72 +1,40 @@
 import os
-import logging
-from telegram import Update, Document
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather on Telegram
+bot_token = '6598445027:AAHOiQ5_P-SV_x0aPH1iuUku6SOboIiMvys'
 
-# States for the conversation
-SELECT_FILE = 1
+def start(update, context):
+    update.message.reply_text("Please send a .txt file containing name:url pairs.")
 
-# Global variable to store the selected file content
-file_content = ""
+def process_txt_file(update, context):
+    file = update.message.document.get_file()
+    file.download('input.txt')
+    with open('input.txt', 'r') as infile:
+        lines = infile.readlines()
 
-# Function to start the bot and ask for a file
-def start(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("Welcome to the bot! Please send a .txt file containing name:url with 'cdn-vidu'.")
+    # Filter lines to keep only those containing 'cdn-vidu'
+    filtered_lines = [line for line in lines if 'cdn-vidu' in line]
 
-    return SELECT_FILE
+    # Prepare the corrected content
+    corrected_content = '\n'.join(filtered_lines)
 
-# Function to handle received documents
-def receive_file(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    document = update.message.document
+    # Write the corrected content to a new file
+    with open('output.txt', 'w') as outfile:
+        outfile.write(corrected_content)
 
-    # Check if the file is a .txt file
-    if document.mime_type == "text/plain":
-        file = context.bot.get_file(document.file_id)
-        file.download("input.txt")
-        
-        with open("input.txt", "r") as txt_file:
-            global file_content
-            file_content = txt_file.read()
+    # Send the corrected file to the user
+    context.bot.send_document(chat_id=update.effective_chat.id, document=open('output.txt', 'rb'))
+    os.remove('input.txt')
+    os.remove('output.txt')
 
-        # Process the file content (remove lines with "cdn-vidu")
-        processed_content = "\n".join(line for line in file_content.split("\n") if "cdn-vidu" in line)
-
-        # Save the processed content to a new file
-        with open("output.txt", "w") as output_file:
-            output_file.write(processed_content)
-
-        # Send the processed file to the user
-        context.bot.send_document(update.message.chat_id, document=open("output.txt", "rb"))
-        return ConversationHandler.END
-    else:
-        update.message.reply_text("Please send a .txt file.")
-        return SELECT_FILE
-
-# Function to handle the cancel command
-def cancel(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("Operation cancelled.")
-    return ConversationHandler.END
-
-def main() -> None:
-    # Initialize the Telegram bot
-    updater = Updater(token="6598445027:AAHOiQ5_P-SV_x0aPH1iuUku6SOboIiMvys", use_context=True)
+def main():
+    updater = Updater(bot_token, use_context=True)
     dp = updater.dispatcher
 
-    # Define the conversation handler
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={SELECT_FILE: [MessageHandler(Filters.document.mime("text/plain"), receive_file)]},
-        fallbacks=[CommandHandler("cancel", cancel)],
-    )
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.document.file_extension('txt'), process_txt_file))
 
-    dp.add_handler(conv_handler)
-
-    # Start the bot
     updater.start_polling()
     updater.idle()
 
